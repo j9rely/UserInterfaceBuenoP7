54c945f63476013c4fe829c0c1508d68cb60dab6
j9rely <145072305+j9rely@users.noreply.github.com>
2024-04-26 15:23:47 -0700

    added text
    scripts text

--- UserInterfaceP7Bueno/Assets/Scripts/GameManager.cs
+++ UserInterfaceP7Bueno/Assets/Scripts/GameManager.cs
@@ -1,25 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+using System.Net.Sockets;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     private float spawnRate = 1.0f;
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    public Button restartButton;
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        isGameActive = true;
         StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
+    }
+    public void GameOver()
+    {
+        isGameActive = false;
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    }
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
     // Update is called once per frame
     void Update()
--- UserInterfaceP7Bueno/Assets/Scripts/Target.cs
+++ UserInterfaceP7Bueno/Assets/Scripts/Target.cs
@@ -10,10 +10,14 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
 
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         targetRb = GetComponent<Rigidbody>();
 
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
@@ -26,6 +30,28 @@ public class Target : MonoBehaviour
     void Update()
     {
 
+    }
+
+    private int GetPointValue()
+    {
+        return pointValue;
+    }
+
+    private void OnMouseDown(int pointValue)
+    {
+        if (gameManager.isGameActive)
+        {
+            
+        }
+        Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(5, pointValue);
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if(!gameObject.CompareTag("Bad")) { gameManager.GameOver(); }
     }
 
     Vector3 RandomForce()
